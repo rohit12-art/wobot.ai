78f2c3a454b5a2c93a4b279cc1d29ae5aff5d5b0
Rohit <rohitrohan25420@gmail.com>
2022-06-21 15:45:52 -0400

    bugs fixed

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -2,30 +2,28 @@ const csv = require("csv-parser");
 const fs = require("fs");
 
 const uploadFile = async (req, res, next) => {
-  console.log(req.file);
-  if (req.files) {
-    var file = req.files.file;
-    console.log(file);
-    var fileName = file.name;
-    file.mv("./uploads/" + fileName, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const result = [];
-        fs.createReadStream("./uploads/" + fileName)
-          .pipe(csv({}))
-          .on("data", (data) => {
-            result.push(data);
-          })
-          .on("end", () => {
-            req.body.fileData = result;
-            next();
-          });
-      }
-    });
+  if (!req.files) {
+    return res.status(400).json("Please attach a file.");
   }
+  var file = req.files.file;
+  var fileName = file.name;
+  file.mv("./uploads/" + fileName, (err) => {
+    if (err) {
+      return res.status(400).json("unexpected Error . Please try again .");
+    } else {
+      const result = [];
+      fs.createReadStream("./uploads/" + fileName)
+        .pipe(csv({}))
+        .on("data", (data) => {
+          result.push(data);
+        })
+        .on("end", () => {
+          req.body.fileData = result;
+          next();
+        });
+    }
+  });
 };
-
 module.exports = {
   uploadFile,
 };
--- model/user.js
+++ model/user.js
@@ -17,15 +17,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
